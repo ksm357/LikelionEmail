c354b4a407992a623e7b56729930893b9d7d8a80
ksm357 <ksm3725@naver.com>
2016-04-16 05:43:14 +0000

    emailservice

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
         mg_client = Mailgun::Client.new("key-db3c1e54124ed4e823f8a3ecc3fdba29")
 
         message_params =  {
-                   from: 'master@likelion.net',
+                   from: 'ksm3725@naver.com',
                    to: @address,
                    subject: @title,
                    text: @content
@@ -20,5 +20,17 @@ class HomeController < ApplicationController
         result = mg_client.send_message('sandbox787f016c83674e2182979f1a2978b123.mailgun.org', message_params).to_h!
         message_id = result['id']
         message = result['message']
+        
+        new_post = Post.new
+        new_post.title = @title
+        new_post.address = @address
+        new_post.content = @content
+        new_post.save
+        
+        redirect_to "/list"
+
+    end
+    def list
+        @email_post = Post.all.order("id desc")
     end
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   get '/' => 'home#index'
   post '/email' => 'home#email'
-
+  get '/list' => 'home#list'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
